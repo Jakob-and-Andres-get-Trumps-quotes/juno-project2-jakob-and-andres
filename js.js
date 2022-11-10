1991a4c76b99455275eeb73bca31402b7df56b13
allenjakob <jsja911@gmail.com>
2022-11-09 19:11:20 -0700

    started JS

--- js.js
+++ js.js
@@ -1,1 +1,18 @@
 trumpApp = {};
+
+fakeArray = ["insert trump quote here","insert trump quote here","insert trump quote here"]
+
+
+trumpApp.random = () => {
+
+};
+
+
+
+
+trumpApp.init = () => {
+    // add all functions here that will be run on startup
+    //trumpApp.random()
+};
+
+trumpApp.init();
