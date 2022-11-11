4beff3283ffbc16928e6dfee82bcbc7f2d012287
allenjakob <jsja911@gmail.com>
2022-11-11 09:50:35 -0700

    added a quote

--- js.js
+++ js.js
@@ -1,11 +1,39 @@
 trumpApp = {};
 
-fakeArray = ["insert trump quote here","insert trump quote here","insert trump quote here"]
+fakeArray = ["That's what they say about people from Mexico, hard workers, but not that smart. Not smart, like me.","insert trump quote here","insert trump quote here"]
 
 
 trumpApp.random = () => {
 
 };
+
+
+
+// this code will be pretty useful for our fakeArray 
+// const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+// function getRandomDay() {
+//   const randomDay = Math.floor(Math.random() * days.length);
+
+//   return days[randomDay];
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
