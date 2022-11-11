5dadaadd47604ef68ac87227a351339bbce6d071
allenjakob <jsja911@gmail.com>
2022-11-11 10:19:22 -0700

    added randomizer and first API call

--- js.js
+++ js.js
@@ -1,11 +1,32 @@
 trumpApp = {};
 
-fakeArray = ["That's what they say about people from Mexico, hard workers, but not that smart. Not smart, like me.","insert trump quote here","insert trump quote here"]
+fakeArray = ["That's what they say about people from Mexico, hard workers, but not that smart. Not smart, like me.", "insert trump quote here", "insert trump quote here"]
+
+const url = 'https://api.tronalddump.io/random/quote'
 
 
 trumpApp.random = () => {
-
+    const randomNum = Math.floor(Math.random() * 2);
+    console.log(randomNum)
+    if (randomNum === 1) {
+        async function getQuote(){
+            const myObject = await fetch(url);
+            const parsedObject = await myObject.json();
+            return parsedObject;
+        }
+        getQuote()
+        .then((quoteData)=>{
+          console.log(quoteData.value)
+        })
+    };
 };
+
+
+// trumpApp.displayQuote = (quote) => {
+//     console.log(quote)
+// }
+
+
 
 
 
@@ -30,17 +51,22 @@ trumpApp.random = () => {
 
 
 
-
-
-
-
-
-
-
-
 trumpApp.init = () => {
     // add all functions here that will be run on startup
-    //trumpApp.random()
+    trumpApp.random()
 };
 
 trumpApp.init();
+
+
+
+///////////////////////////////////////////////////////////////////////
+
+// fetch(url)
+//         .then((res) => {
+//             console.log(res.json())
+//             return res.json()
+//         .then( (data) => {
+//             trumpApp.displayQuote(data.value)
+//             })
+//         })
